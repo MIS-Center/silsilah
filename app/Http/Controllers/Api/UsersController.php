f84b95d7706fc2ca0755102d4e5bcdcf66b6e9df
MIS-1 <mis.developing@hotmail.com>
2022-06-08 01:53:34 +0300

    set findChilds to be for grand_father for tree api

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -137,10 +137,14 @@ class UsersController extends Controller
 
         $f = $user->father;
         $m = $user->mother;
+        $grand_father = $user ;
 
         while( !empty($f) ){
             array_push($fathers,$f) ;
             $f = $f->father;
+            if(!empty($f)){
+                $grand_father = $f;
+            }
         }
 
         while( !empty($m) ){
@@ -148,7 +152,7 @@ class UsersController extends Controller
             $m = $m->mother;
         }
 
-        $this->findChilds($user) ;
+        $this->findChilds($grand_father) ;
 
         $tree = [
             'user' => $user,

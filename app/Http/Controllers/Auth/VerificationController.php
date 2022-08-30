8741576f4d68c8981645a31ae37ac3b7a209302c
MIS-1 <mis.developing@hotmail.com>
2022-08-30 04:50:47 +0300

    add event for Registred to make api send verfication email as web version

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -71,35 +71,16 @@ class VerificationController extends Controller
      */
     public function verify(Request $request)
     {
-
         $user = User::find($request->route('id'));
 
-        // if (! hash_equals((string) $request->route('id'), (string) $request->user()->getKey())) {
-        //     throw new AuthorizationException;
-        // }
-
-        if (! hash_equals((string) $request->route('hash'), sha1($request->user()->getEmailForVerification()))) {
+        if (!hash_equals((string) $request->route('hash'), sha1($user->getEmailForVerification()))) {
             throw new AuthorizationException;
         }
-
-        if ($request->user()->hasVerifiedEmail()) {
-            return $request->wantsJson()
-                        ? new JsonResponse([], 204)
-                        : redirect($this->redirectPath());
-        }
-
-        if ($request->user()->markEmailAsVerified()) {
-            event(new Verified($request->user()));
-        }
-
-        if ($response = $this->verified($request)) {
-            return $response;
-        }
-
-    //    return "done " ;
-        return $request->wantsJson()
-                    ? new JsonResponse([], 204)
-                    : redirect($this->redirectPath())->with('verified', true);
+    
+        if ($user->markEmailAsVerified())
+            event(new Verified($user));
+    
+        return redirect($this->redirectPath())->with('verified', true);
 
 
     }
